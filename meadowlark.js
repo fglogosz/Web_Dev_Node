13ec497595557de5562f56be321b9874f24f224c
fglogosz <fglogosz@gmail.com>
2024-06-04 11:42:01 +0200

    add uploading file

--- meadowlark.js
+++ meadowlark.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { engine: expressHandlebars } = require("express-handlebars");
-const fortune = require("./lib/fortune");
+const bodyParser = require("body-parser");
+const multiparty = require("multiparty");
+
 const handlers = require("./lib/handlers");
-const weatherMiddleware = require("./lib/middleware/weather");
-const bodyPareser = require("body-parser");
+const weatherMiddlware = require("./lib/middleware/weather");
 
 const app = express();
 
@@ -23,20 +24,51 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(bodyPareser.urlencoded({ extended: true }));
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
-app.use(weatherMiddleware);
+app.use(weatherMiddlware);
 
 app.get("/", handlers.home);
-app.get("/section-test", handlers.sectionTest);
 
+// handlers for browser-based form submission
 app.get("/newsletter-signup", handlers.newsletterSignup);
 app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
 app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
+
+// handlers for fetch/JSON form submission
+app.get("/newsletter", handlers.newsletter);
+app.post("/api/newsletter-signup", handlers.api.newsletterSignup);
+
+// vacation photo contest
+app.get("/contest/vacation-photo", handlers.vacationPhotoContest);
+app.get("/contest/vacation-photo-ajax", handlers.vacationPhotoContestAjax);
+app.post("/contest/vacation-photo/:year/:month", (req, res) => {
+	const form = new multiparty.Form();
+	form.parse(req, (err, fields, files) => {
+		if (err)
+			return handlers.vacationPhotoContestProcessError(req, res, err.message);
+		console.log("got fields: ", fields);
+		console.log("and files: ", files);
+		handlers.vacationPhotoContestProcess(req, res, fields, files);
+	});
+});
+app.get(
+	"/contest/vacation-photo-thank-you",
+	handlers.vacationPhotoContestProcessThankYou
+);
+app.post("/api/vacation-photo-contest/:year/:month", (req, res) => {
+	const form = new multiparty.Form();
+	form.parse(req, (err, fields, files) => {
+		if (err)
+			return handlers.api.vacationPhotoContestError(req, res, err.message);
+		handlers.api.vacationPhotoContest(req, res, fields, files);
+	});
+});
 
 app.use(handlers.notFound);
 app.use(handlers.serverError);
