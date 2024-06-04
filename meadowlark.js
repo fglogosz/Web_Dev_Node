91ceeaba33085d54010bad14fbf984a4a43c22df
fglogosz <fglogosz@gmail.com>
2024-06-04 16:15:46 +0200

    cookies and session

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,11 @@ const express = require("express");
 const { engine: expressHandlebars } = require("express-handlebars");
 const bodyParser = require("body-parser");
 const multiparty = require("multiparty");
+const cookieParser = require("cookie-parser")
+const expressSession = require("express-session")
 
+const flashMiddleware = require("./lib/middleware/flash")
+const { credentials } = require("./config")
 const handlers = require("./lib/handlers");
 const weatherMiddlware = require("./lib/middleware/weather");
 
@@ -27,11 +31,19 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(cookieParser(credentials.cookieSecret))
+app.use(expressSession({
+	resave: false,
+	saveUninitialized: false,
+	secret: credentials.cookieSecret,
+}))
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
 app.use(weatherMiddlware);
+app.use(flashMiddleware)
 
 app.get("/", handlers.home);
 
@@ -39,10 +51,11 @@ app.get("/", handlers.home);
 app.get("/newsletter-signup", handlers.newsletterSignup);
 app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
 app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
+app.get("/newsletter-archive", handlers.newsletterSignupThankYou);
 
 // handlers for fetch/JSON form submission
-app.get("/newsletter", handlers.newsletter);
-app.post("/api/newsletter-signup", handlers.api.newsletterSignup);
+// app.get("/newsletter", handlers.newsletter);
+// app.post("/api/newsletter-signup", handlers.api.newsletterSignup);
 
 // vacation photo contest
 app.get("/contest/vacation-photo", handlers.vacationPhotoContest);
